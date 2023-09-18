91027f0686a465977391e60f6831cacebc83dbc0
JuanCarlosAcostaPeraba <juancarlos.ap.dev@gmail.com>
2023-09-18 11:42:39 +0100

    7.1.2 - Tarea 2. Comunicación serie

--- pract-1/712-t2-comunicacion-serie.ino
+++ pract-1/712-t2-comunicacion-serie.ino
@@ -253,10 +253,7 @@ void select_hexadecimal()
 	{
 		print2 = false;
 		Serial.println("Seleccione un caracter hexadecimal entre los siguientes:");
-		for (int i = 0; i < 16; i++)
-		{
-			Serial.print(hexadecimal[i] + " ");
-		}
+		Serial.print("0, 1, 2, 3, 4, 5, 6, 7, 8, 9, A, B, C, D, E, F");
 		Serial.println();
 	}
 	int hex = Serial.read();
