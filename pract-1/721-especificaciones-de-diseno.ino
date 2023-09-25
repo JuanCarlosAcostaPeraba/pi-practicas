530577238f0907d83b985bb5aec61390f982031f
JuanCarlosAcostaPeraba <juancarlos.ap.dev@gmail.com>
2023-09-25 19:01:28 +0100

    7.2.1 - Especificaciones de diseño

--- pract-1/721-especificaciones-de-diseno.ino
+++ pract-1/721-especificaciones-de-diseno.ino
@@ -26,3 +26,60 @@ La aplicación se implementará de acuerdo a las siguientes especificaciones:
 	2.- Modo reducido-inferior de visualización (dos dígitos): OFF-OFF-decenas-unidades
 	3.- Modo reducido-superior de visualización (dos dígitos): decenas-unidades-OFF-OFF
 */
+
+// Pulsadores
+#define PRIGHT 30	 // PC[7] pulsador right
+#define PDOWN 31	 // PC[6] pulsador down
+#define PLEFT 32	 // PC[5] pulsador left
+#define PSELECT 33 // PC[4] pulsador select/enter
+#define PUP 34		 // PC[3] pulsador up
+#define PSTART 37	 // PC[0] speaker
+
+// Display 8 segmentos
+#define D4 49 // Pin 49 - unidades
+#define D3 48 // Pin 48 - decenas
+#define D2 47 // Pin 47 - centenas
+#define D1 46 // Pin 46 - unidades de millar
+
+// Matriz display 8 segmentos
+char display_map[4] = {D4, D3, D2, D1};
+
+// Matriz valores hexadecimales
+char hexadecimal[16] = {
+		'0', '1', '2', '3',
+		'4', '5', '6', '7',
+		'8', '9', 'A', 'B',
+		'C', 'D', 'E', 'F'};
+
+// Matriz valores hexadecimales en binario
+char hex_value[16] = {
+		0x3F, 0x06, 0x5B, 0x4F,
+		0x66, 0x6D, 0x7D, 0x07,
+		0x7F, 0x6F, 0x77, 0x7C,
+		0x39, 0x5E, 0x79, 0x71};
+
+// Matriz teclado
+char teclado_map[][3] = {
+		{'1', '2', '3'},
+		{'4', '5', '6'},
+		{'7', '8', '9'},
+		{'*', '0', '#'}};
+
+void setup()
+{
+	Serial.begin(9600); // Inicializamos el puerto serie
+
+	// Puerto A salida
+	DDRA = B11111111;	 // Configuramos el puerto A como salida (0xFF)
+	PORTA = B11111111; // Inicializamos el puerto A a 1 (0xFF)
+
+	// Puerto L teclado
+	DDRL = B00001111;	 // Configuramos los pines 0, 1, 2 y 3 del puerto L como entrada, y el resto como salida (0x0F)
+	PORTL = B11111111; // Inicializamos el puerto L a 1 (0xFF)
+
+	// Puerto C
+	DDRC = B00000000;	 // Configuramos el pin 0 del puerto C como entrada (0x00)
+	PORTC = B11111111; // Inicializamos el puerto C a 1 (0cFF)
+}
+
+void loop() {}
