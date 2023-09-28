98d8683e32b0969db67ecb9aa3b844d985f5a135
JuanCarlosAcostaPeraba <juancarlos.ap.dev@gmail.com>
2023-09-28 17:38:03 +0100

    7.2.1 - Especificaciones de diseño

--- pract-1/721-especificaciones-de-diseno.ino
+++ pract-1/721-especificaciones-de-diseno.ino
@@ -330,19 +330,15 @@ void keyboard(int column)
 	{
 	case 7:
 		Serial.print(teclado_map[0][column]);
-		Serial.print(column);
 		break;
 	case 11:
 		Serial.print(teclado_map[1][column]);
-		Serial.print(column);
 		break;
 	case 13:
 		Serial.print(teclado_map[2][column]);
-		Serial.print(column);
 		break;
 	case 14:
 		Serial.print(teclado_map[3][column]);
-		Serial.print(column);
 		break;
 	}
 }
