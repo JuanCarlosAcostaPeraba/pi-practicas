676e9d54239e6d7f53a0786b21d2da7e221a3adc
JuanCarlosAcostaPeraba <juancarlos.ap.dev@gmail.com>
2023-10-07 11:05:18 +0100

    4.1.1 - Tarea 1. Generación de señales en modo NORMAL

---
+++ pract-2/411-t1-generacion-de-senales-en-modo-normal.ino
@@ -0,0 +1,7 @@
+/*
+Generar dos señales de onda cuadrada desfasadas 90º eléctricos
+con el timer 3 utilizando las salidas OC3A (pin 5) y OC3C (pin 3)
+de periodo T = 65,536 ms (f = 1/T = 15,2587 Hz). Comprobar que
+las señales generadas por el timer son las correctas haciendo
+uso del osciloscopio del simulador Proteus.
+*/
